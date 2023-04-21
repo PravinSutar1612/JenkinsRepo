05893de95d39516ba4180f3a4f1ef240cd6bb694
Pravin Sutar <128134416+PravinSutar1612@users.noreply.github.com>
2023-04-21 11:11:57 +0530

    Add files via upload

---
+++ Question7PATTERN1.java
@@ -0,0 +1,23 @@
+package Assignment2;
+/*(1)
+ *   **********
+ *   **********
+ *   **********
+ */
+public class Question7PATTERN1 
+{
+
+	public static void main(String[] args) 
+	{
+		 for(int i=1;i<=3;i++) //row
+	        {
+		           for(int j=1;j<=10;j++)  //column
+		                 {
+		                        System.out.print("*");	  
+		                 }
+		                 System.out.println();
+	        }     
+
+	}
+
+}
